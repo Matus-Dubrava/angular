218d0282dbf28ef6e032f3974d83add0cb3ae713
matus dubrava <matusdubrava@gmail.com>
2018-09-26 23:57:45 +0200

    added directives to project

--- tutorials/first-app/src/app/servers/servers.component.ts
+++ tutorials/first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() { 
     setTimeout(() => {
@@ -24,7 +26,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
   }
 
 }
